f825ec69c0a20904c5ca5a1040979b2709732e7d
Roger <1584@holbertonschool.com>
2020-05-13 11:15:58 -0500

    add function push, swap, pall, pint, nop

---
+++ Functions.c
@@ -0,0 +1,97 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "monty.h"
+
+/**
+ * push - add node initial of a list
+ * @head: Pointer from main
+ * @n: Integer to node
+ *
+ * Return: head
+ */
+
+void *func_push(stack_t **head, unsigned int n)
+{
+	stack_t *new_node;
+
+	if (head == NULL)
+		return (NULL);
+
+	new_node = malloc(sizeof(stack_t));
+	if (!new_node)
+		return (NULL);
+	if (new_node == NULL)
+		return (NULL);
+	new_node->n = n;
+	new_node->prev = NULL;
+	new_node->next = *head;
+
+	if (*head)
+		(*head)->prev = new_node;
+	*head = new_node;
+}
+
+/**
+ * pall - for managment of a list
+ * @h: poniter
+ * Return: forward Value Counter.
+*/
+
+void func_pall(const stack_t *h, unsigned int n)
+{
+	int forward = 0;
+
+	while (h)
+	{
+		printf("%d\n", h->n);
+		forward++;
+		h = h->next;
+	}
+}
+
+/**
+ * pint - for managment of a list
+ * @h: poniter
+ * Return: forward Value Counter.
+*/
+
+void func_pint(const stack_t *h, unsigned int n)
+{
+	printf("%d\n", h->n);
+}
+
+/**
+ * swap - Swaps first two elements.
+ * @stack: pointer node.
+ * @line_number: number in file.
+ */
+
+void func_swap(stack_t **stack, unsigned int line_number)
+{	
+
+	stack_t *temp, *node;
+	int h;
+
+	if (!stack || !*stack || !(*stack)->next)
+	{
+		printf("L%d: can't swap, stack too short\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+	else
+	{
+		temp = *stack;
+		node = temp->next;
+		h = temp->n;
+		temp->n = node->n;
+		node->n = h;
+	}
+}
+
+void func_nop(stack_t **stack, unsigned int line_number)
+{
+	(void)stack;
+	(void)line_number;
+}
+
+
--- monty.h
+++ monty.h
@@ -15,6 +15,9 @@ typedef struct stack_s
         struct stack_s *prev;
         struct stack_s *next;
 } stack_t;
+
+
+
 /**
  * struct instruction_s - opcode and its function
  * @opcode: the opcode
@@ -28,4 +31,8 @@ typedef struct instruction_s
         char *opcode;
         void (*f)(stack_t **stack, unsigned int line_number);
 } instruction_t;
+
+
+
+
 #endif // MONTY_H
