f092bd52f2152adc45f807184e34fe95926dd305
Roger <1584@holbertonschool.com>
2020-05-14 10:25:38 -0500

    correct declaration global variable

--- monty.h
+++ monty.h
@@ -7,10 +7,7 @@
 #include <string.h>
 #include <ctype.h>
 
-int exit_value;
-int stack_value;
-
-
+extern int exit_and_Svalue[];
 /**
  * struct stack_s - doubly linked list representation of a stack (or queue)
  * @n: integer
@@ -22,9 +19,9 @@ int stack_value;
  */
 typedef struct stack_s
 {
-        int n;
-        struct stack_s *prev;
-        struct stack_s *next;
+	int n;
+	struct stack_s *prev;
+	struct stack_s *next;
 } stack_t;
 
 /**
@@ -37,37 +34,31 @@ typedef struct stack_s
  */
 typedef struct instruction_s
 {
-        char *opcode;
-        void (*f)(stack_t **stack, unsigned int line_number);
+	char *opcode;
+	void (*f)(stack_t **stack, unsigned int line_number);
 } instruction_t;
-
 
 char **split_line(char *line, unsigned int numlinea);
 int find_function(char **tokens, unsigned int numlinea, stack_t **head);
-
 void func_pop(stack_t **head, unsigned int numlinea);
 int delete_dnodeint_at_index(stack_t **head, unsigned int index);
 size_t dlistint_len(const stack_t *h);
-
 void func_push(stack_t **head, unsigned int n);
 stack_t *add_dnodeint(stack_t **head, const int n);
 stack_t	*sum_dlistint(stack_t **head);
-
 void func_pall(stack_t **stack, unsigned int line_number);
 void func_pint(stack_t **stack, unsigned int line_number);
 void func_swap(stack_t **stack, unsigned int line_number);
 void func_nop(stack_t **stack, unsigned int line_number);
 void func_add(stack_t **head, unsigned int numlinea);
-
 void free_dlistint(stack_t *head);
 size_t print_dlistint(const stack_t *h);
-
 void func_add(stack_t **stack, unsigned int line_number);
 void func_sub(stack_t **stack, unsigned int line_number);
 void func_div(stack_t **stack, unsigned int line_number);
 void func_mul(stack_t **stack, unsigned int line_number);
 void func_mod(stack_t **head, unsigned int line_number);
 void func_pchar(stack_t **stack, unsigned int line_number);
-
-
-#endif // MONTY_H
+int check_spaces(char *line);
+int isNumber(char *ptr);
+#endif
