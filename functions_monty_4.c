45d46083bc4e2b013e9a22a25694bf8e4fa7b07c
Roger <1584@holbertonschool.com>
2020-05-14 18:31:56 -0500

    add description for function monty 4

--- functions_monty_4.c
+++ functions_monty_4.c
@@ -10,6 +10,12 @@ void func_comments(stack_t **stack, unsigned int line_number)
 	(void)stack;
 	exit_and_Svalue[0] = 0;
 }
+
+/**
+ * func_stack - sets the format of the data to a stack (LIFO)
+ * @stack: Pointer to the head of the list
+ * @line_number: An int that count each line
+ */
 void func_stack(stack_t **stack, unsigned int line_number)
 {
 	exit_and_Svalue[2] = 0;
@@ -17,10 +23,15 @@ void func_stack(stack_t **stack, unsigned int line_number)
 	(void)line_number;
 
 }
+
+/**
+ * func_queue - sets the format of the data to a queue (FIFO)
+ * @stack: Pointer to the head of the list
+ * @line_number: An int that count each line
+ */
 void func_queue(stack_t **stack, unsigned int line_number)
 {
 	exit_and_Svalue[2] = 1;
 	(void)stack;
 	(void)line_number;
 }
-
